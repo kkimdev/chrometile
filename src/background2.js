3bfc6707a6033e7bc1cb4512376c6100a3326bc8
Kibeom Kim <kkimdev@users.noreply.github.com>
2022-12-22 14:52:27 -0800

    .

--- src/background2.js
+++ src/background2.js
@@ -16,6 +16,11 @@ const commands = new Map([
     ["99009-place-9", () => place(9)]
 ]);
 
+function logger(...msgs) {
+    // console.log(Error().stack);
+    console.log('\t'.repeat(Error().stack.split('\n').length - 2), ...msgs);
+}
+
 function computeCenter(box) {
     return {
         x: box.left + box.width / 2,
@@ -27,11 +32,13 @@ function computeDistance(point1, point2) {
     return (point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2;
 }
 
-function getDisplay(window) {
-    // TOOD: early return if length is 1.
+function getClosestDisplay(window) {
+    if (chromeSystemDisplayGetInfoCache.length === 1)
+        return chromeSystemDisplayGetInfoCache[0];
+
     const windowCenter = computeCenter(window);
-    // console.log(displayInfos);
-    // console.log('windowCenter', windowCenter);
+    // logger(displayInfos);
+    // logger('windowCenter', windowCenter);
 
     // TODO: minimum distance logic might not be the best algorithm.
     let minDistance = Infinity;
@@ -39,8 +46,8 @@ function getDisplay(window) {
 
     for (const displayInfo of chromeSystemDisplayGetInfoCache) {
         const distance = computeDistance(windowCenter, computeCenter(displayInfo.workArea));
-        // console.log('displayCenter', computeCenter(displayInfo.workArea));
-        // console.log('distance', distance);
+        // logger('displayCenter', computeCenter(displayInfo.workArea));
+        // logger('distance', distance);
         if (distance < minDistance) {
             minDistance = distance;
             minDistanceWindow = displayInfo;
@@ -52,7 +59,7 @@ function getDisplay(window) {
 
 async function place(positionNumber) {
     const focusedWindow = await chrome.windows.getLastFocused();
-    const display = getDisplay(focusedWindow);
+    const display = getClosestDisplay(focusedWindow);
     const displayWorkArea = display.workArea;
 
     let left = displayWorkArea.left;
@@ -68,52 +75,20 @@ async function place(positionNumber) {
         width = displayWorkArea.width / 2;
     if ([7, 8, 9, 1, 2, 3].includes(positionNumber))
         height = displayWorkArea.height / 2;
-    
-    const placingBounds = { 'top': top, 'left': left, 'width': width, 'height': height, state: "normal" };
-    console.log(`Placing window ${focusedWindow.id} to ${JSON.stringify(placingBounds)}`);
+
+    const placingBounds = {
+        'top': Math.round(top),
+        'left': Math.round(left),
+        'width': Math.round(width),
+        'height': Math.round(height),
+        state: "normal"
+    };
+    logger("Placing window", focusedWindow, "to", placingBounds);
     chrome.windows.update(focusedWindow.id, placingBounds);
 }
 
-// // Used only for initialization
-// async function updateAll() {
-//     const allWindows = await chrome.windows.getAll();
-
-//     // console.log(displayInfos);
-//     // console.log(allWindows);
-
-//     scaledWindowBounds = {};
-//     for (const window of allWindows) {
-//         addWindow(window);
-//     }
-
-//     // TODO: create window to display map or
-//     //       create window relative scaled position.
-//     // 
-// }
-
-// async function repositionWindows() {
-//     console.log(`start repositionWindows`);
-//     const allWindows = await chrome.windows.getAll();
-
-//     for (const window of allWindows) {
-//         const closestDisplay = getDisplay(window);
-//         const scaledBound = scaledWindowBounds[window.id];
-//         if (!scaledBound) continue;
-
-//         chrome.windows.update(window.id, {
-//             left: Math.round(scaledBound.left * closestDisplay.workArea.width),
-//             width: Math.round(scaledBound.width * closestDisplay.workArea.width),
-//             top: Math.round(scaledBound.top * closestDisplay.workArea.height),
-//             height: Math.round(scaledBound.height * closestDisplay.workArea.height),
-//             state: "normal"
-//         });
-//     }
-//     console.log(`end repositionWindows`);
-// }
-////////////////////////////////////////////////////////////////////////////////
-
 async function addAllWindows() {
-    console.log("addAllWindows");
+    logger("addAllWindows");
     const allWindows = await chrome.windows.getAll();
     chromeSystemDisplayGetInfoCache = await chrome.system.display.getInfo();
     for (const window of allWindows) {
@@ -122,24 +97,26 @@ async function addAllWindows() {
 }
 
 function addWindow(window) {
-    console.log("addWindow");
-    const closestDisplay = getDisplay(window);
-    scaledWindowBounds[window.id] = {
+    const closestDisplay = getClosestDisplay(window);
+    const newScaledWindowBound = {
         displayChangedVersion: displayChangedVersion,
         left: window.left / closestDisplay.workArea.width,
         width: window.width / closestDisplay.workArea.width,
         top: window.top / closestDisplay.workArea.height,
         height: window.height / closestDisplay.workArea.height,
     };
+
+    logger("addWindow", window, "from", scaledWindowBounds[window.id], "to", newScaledWindowBound);
+    scaledWindowBounds[window.id] = newScaledWindowBound;
 }
 
 function updateWindow(window) {
-    console.log("updateWindow");
-    console.log(scaledWindowBounds[window.id]['displayChangedVersion'], displayChangedVersion);
+    logger("updateWindow", window);
     if (scaledWindowBounds[window.id]['displayChangedVersion'] < displayChangedVersion) {
+        logger("updateWindow ignored because displayChangedVersion is lower", scaledWindowBounds[window.id]['displayChangedVersion'], "<", displayChangedVersion);
         return;
     }
-    
+
     addWindow(window);
 }
 
@@ -147,67 +124,72 @@ function removeWindow(windowId) {
     delete scaledWindowBounds[windowId];
 }
 
-async function repositionWindows() {
-    console.log(`start repositionWindows`);
+async function repositionWindows(version) {
+    logger("repositionWindows start, version:", version);
+    chromeSystemDisplayGetInfoCache = await chrome.system.display.getInfo();
 
     for (const [windowId, value] of Object.entries(scaledWindowBounds)) {
         const scaledBound = scaledWindowBounds[windowId];
         const window = await chrome.windows.get(parseInt(windowId));
-        const closestDisplay = getDisplay(window);
-        
-        console.log(scaledBound['displayChangedVersion'], displayChangedVersion);
-        if (scaledBound['displayChangedVersion'] < displayChangedVersion) {
-            chrome.windows.update(window.id, {
-                left: Math.round(scaledBound.left * closestDisplay.workArea.width),
-                width: Math.round(scaledBound.width * closestDisplay.workArea.width),
-                top: Math.round(scaledBound.top * closestDisplay.workArea.height),
-                height: Math.round(scaledBound.height * closestDisplay.workArea.height),
-                state: "normal"
-            });
-            addWindow(window);
-        }
-    }
-    console.log(`end repositionWindows`);
-}
-////////////////////////////////////////////////////////////////////////////////
+        const closestDisplay = getClosestDisplay(window);
 
-chrome.commands.onCommand.addListener((command) => {
-    console.log('Command received:', command);
-    commands.get(command)();
-});
+        if (version < displayChangedVersion)
+            break;
 
-chrome.system.display.onDisplayChanged.addListener(
-    async () => {
-        console.log("onDisplayChanged");
-        chromeSystemDisplayGetInfoCache = await chrome.system.display.getInfo();
-        displayChangedVersion += 1;
-        repositionWindows();
-    }
-);
+        console.assert(scaledBound['displayChangedVersion'] < displayChangedVersion)
 
-chrome.windows.onBoundsChanged.addListener(
-    (window) => {
-        // TODO: Keeping window aspect ratio logic.
-        console.log(`onBoundsChanged ${JSON.stringify(window)}`);
-        updateWindow(window);
-        // updateAll();
-    }
-);
+        const newBound = {
+            left: Math.round(scaledBound.left * closestDisplay.workArea.width),
+            width: Math.round(scaledBound.width * closestDisplay.workArea.width),
+            top: Math.round(scaledBound.top * closestDisplay.workArea.height),
+            height: Math.round(scaledBound.height * closestDisplay.workArea.height),
+            state: "normal"
+        };
 
-chrome.windows.onCreated.addListener(
-    (window) => {
-        console.log(`onCreated ${JSON.stringify(window)}`);
+        logger("repositionWindows update window:", window, "from", scaledBound, "to", newBound);
+        chrome.windows.update(window.id, newBound);
         addWindow(window);
     }
-);
+    logger("repositionWindows end, version:", version);
+}
 
-chrome.windows.onRemoved.addListener(
-    (windowId) => {
-        console.log(`onRemoved ${windowId}`);
-        removeWindow(windowId);
-    }
-);
+function addListeners() {
+    chrome.commands.onCommand.addListener((command) => {
+        logger('Command received:', command);
+        commands.get(command)();
+    });
 
-////////////////////////////////////////////////////////////////////////////////
+    chrome.system.display.onDisplayChanged.addListener(
+        () => {
+            logger("chrome.system.display.onDisplayChanged");
+            displayChangedVersion += 1;
+            repositionWindows(displayChangedVersion);
+        }
+    );
 
+    chrome.windows.onBoundsChanged.addListener(
+        (window) => {
+            logger("chrome.windows.onBoundsChanged", window);
+            updateWindow(window);
+        }
+    );
+
+    chrome.windows.onCreated.addListener(
+        (window) => {
+            logger("chrome.windows.onCreated", window);
+            addWindow(window);
+        }
+    );
+
+    chrome.windows.onRemoved.addListener(
+        (windowId) => {
+            logger("chrome.windows.onRemoved", windowId);
+            removeWindow(windowId);
+        }
+    );
+}
+
+addListeners();
+// TODO: Need to ensure `addAllWindows()` is finished before listener handling.
+//       Though it will be the case 99.99% times in practice already.
 addAllWindows();
